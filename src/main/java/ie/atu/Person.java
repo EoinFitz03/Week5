268b71df0adb251aca7c6a719d4254fa2a17a197
EoinFitz03 <G00418325@atu.ie>
2023-10-23 09:54:52 +0100

    added new file

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -2,18 +2,49 @@ package ie.atu;
 
 public class Person {
 
+    public String getFirstName() {
+        return firstName;
+    }
 
-    public Person(String firstName) {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+
+
+
+
+
+
     public Person() {
     this.firstName="";
-        this.lastName="";
-        this.age=0;
+    this.lastName="";
+    this.age=0;
     }
 
     private String firstName;
-
 
     private String lastName;
     private int age;
